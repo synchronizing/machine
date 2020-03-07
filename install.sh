0a5f42b9e32e287e70ec0c1c4929ca5d9d6f3db6
Felipe Faria <felipefaria@me.com>
2020-03-06 22:19:18 -0500

    Modified the installation.

--- install.sh
+++ install.sh
@@ -21,9 +21,11 @@ clear
 logo
 
 # Begins Installatin
-echo -e "Please select your system:\n"
-echo -e " 1. Mac OSX"
-echo -e " 2. Ubuntu\n"
+echo -e "Please select type of installation:\n"
+echo -e " 1. Minimun"
+echo -e " 2. General\n"
+echo -e " 3. OSX\n"
+echo -e " 4. Ubuntu\n"
 
 read -p "Which installation to use? " -n 1 -r
 echo -e "\n"
@@ -32,12 +34,20 @@ sh ~/.machine/scripts/general.sh
 
 if [[ $REPLY =~ ^[1]$ ]]
 then
-  echo -e "Installing for MacOSX."
-  sh ~/.machine/install/platform/osx.sh
+  echo -e "Installing minimun."
+  sh ~/.machine/scripts/minimun.sh
 elif [[ $REPLY =~ ^[2]$ ]]
 then
+  echo -e "Installing general."
+  sh ~/.machine/scripts/general.sh
+elif [[ $REPLY =~ ^[3]$ ]]
+then
+  echo -e "Installing for OSX."
+  sh ~/.machine/scripts/osx.sh
+elif [[ $REPLY =~ ^[3]$ ]]
+then
   echo -e "Installing for Ubuntu."
-  sh ~/.machine/scripts/platform/linux.sh
+  sh ~/.machine/scripts/linux.sh
 else
   echo -e "Nothing was selected."
   echo -e "Exiting with error 1.\n"
